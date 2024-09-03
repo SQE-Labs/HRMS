5bcb35c55e8579a7d21316d98aa7b0b07be701de
Aniket159015 <aniket.joshi@sqelabs.com>
2024-09-03 13:30:37 +0530

    new commit

--- cypress/e2e/pages/Dashboard.js
+++ cypress/e2e/pages/Dashboard.js
@@ -1,11 +1,7 @@
 import BasePage from "./BasePage";
 
 class Dashboard extends BasePage{
-
-    constructor(){
-        super();
-    }
-
+    
     // Locators
     get logoutButton() { return cy.get('.log-out > .text-center')}
 
