b53a529b8e1137d2f233a24afcfc391b5dcec456
Sandy-N <sandeep.negi@sqelabs.com>
2024-09-04 12:49:45 +0530

    Delete node_modules directory

--- node_modules/cypress-mochawesome-reporter/lib/consts.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  defaultHtmlOutputFolder: 'cypress/reports/html',
-  defaultConfigOutput: 'cypress/.tmp/cypressMochawesomeReporterConfig.json',
-  cucumberStepsContextKey: '__cucumber_source__',
-};
