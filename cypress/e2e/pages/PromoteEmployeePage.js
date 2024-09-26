8f417328e5011070f5d8d6c2533c77cdc3ddf044
parveen9719 <parveen.sharma@sqelabs.com>
2024-09-26 17:13:32 +0530

    new test script

--- cypress/e2e/pages/PromoteEmployeePage.js
+++ cypress/e2e/pages/PromoteEmployeePage.js
@@ -4,11 +4,39 @@ class PromoteEmployeePage extends BasePage {
 
 
 //Locators
-get promoteEmployeeLbl() {return cy.get("h3.heading")}
+get promoteEmployeeLbl() {return cy.get("div.page-heading h1")}
 get selectEmployeeDrp(){return cy.get("#react-select-2-input")}
 get tableHeadersLbl(){return cy.get("table.resume tr th")}
+get promoteBtn(){return cy.get("button[data-bs-target='#staticBackdropPromote']")}
+get cancelBtn(){return cy.get("div.justify-content-center button[type='button']")}
+get submitBtn(){return cy.get("div.justify-content-center button[type='submit']")}
+get promotePopHeaderLbl(){return cy.get("#staticBackdropLabel")}
+get crossIconBtn(){return cy.get("button[aria-label='Close'].btn-close")}
+get selectDepartmentDrp(){return cy.get("#department")}
+get selectDesignationDrp(){return cy.get("#designation")}
 
 //Methods
+
+clickOnPromote(){
+    this.promoteBtn.click();
+}
+
+clickOnCancel(){
+    this.cancelBtn.wait(500).click();
+}
+
+clickOnCrossIcon(){
+    this.crossIconBtn.wait(500).click();
+}
+
+clickOnSubmitBtn(){
+    this.submitBtn.wait(500).click();
+}
+
+
+getdesgnationOptions(){
+    return this.selectDesignationDrp.find('option').should('have.length',6)
+}
 
 }
 export default new PromoteEmployeePage();
---
+++ cypress/e2e/tests/PromoteEmployeeTests.cy.js
@@ -0,0 +1,103 @@
+import sideBar from "../components/SideBar";
+import PromoteEmployeePage from "../pages/PromoteEmployeePage";
+import { selectDrpValueByText } from '../../support/utils';
+
+
+beforeEach(() => {
+
+    // login to Application
+    cy.login();
+    sideBar.navigateTo("Employee Management", "Promote Employee");
+    cy.wait(1000);
+
+})
+
+
+describe("Employee Managment Promote Employee Tests", () => {
+
+    it("HRMIS_1: Verify Promote Open when user click on Promote Employee Subtab under Employee Managment", () => {
+
+        const expectedTexts = ['Employee Id', 'Name', 'Department', 'Designation' ,'Action'];
+
+        PromoteEmployeePage.promoteEmployeeLbl.invoke('text').then((text)=>{
+            expect(text.trim()).to.eq('Promote Employee');
+        });
+        cy.wait(1000);
+        selectDrpValueByText(PromoteEmployeePage.selectEmployeeDrp, "Auto Mation User", true, PromoteEmployeePage.selectEmployeeDrp);
+        PromoteEmployeePage.tableHeadersLbl.each((headerText, index) => {
+            cy.wrap(headerText).should('contain.text', expectedTexts[index])
+        });
+    });
+
+
+    it("HRMIS_2: Verify Promot pop up open and close when user click on Promot and Cancel button", () => {
+
+        cy.wait(1000);
+        selectDrpValueByText(PromoteEmployeePage.selectEmployeeDrp, "Auto Mation User", true, PromoteEmployeePage.selectEmployeeDrp);
+        PromoteEmployeePage.clickOnPromote();
+        PromoteEmployeePage.promotePopHeaderLbl.should('have.text','Promote Employee');
+        PromoteEmployeePage.clickOnCancel();
+        PromoteEmployeePage.promotePopHeaderLbl.should('not.be.visible');
+
+        // Verify Crosse Icon on Pop up
+        PromoteEmployeePage.clickOnPromote();
+        PromoteEmployeePage.clickOnCrossIcon();
+        PromoteEmployeePage.promotePopHeaderLbl.should('not.be.visible');
+
+
+    });
+
+
+    it("HRMIS_3: Verify Validation message appears after clicking on submit button when no department or designation is selected", () => {
+        
+        cy.wait(1000);
+        selectDrpValueByText(PromoteEmployeePage.selectEmployeeDrp, "Auto Mation User", true, PromoteEmployeePage.selectEmployeeDrp);
+        PromoteEmployeePage.clickOnPromote();
+        PromoteEmployeePage.promotePopHeaderLbl.should('have.text','Promote Employee');
+
+        // When no department is selected.
+        PromoteEmployeePage.clickOnSubmitBtn();
+        PromoteEmployeePage.selectDepartmentDrp
+        .invoke('prop', 'validationMessage') 
+        .should('equal', 'Please select an item in the list.');
+
+        // select department
+
+        selectDrpValueByText(PromoteEmployeePage.selectDepartmentDrp, "GENRIC", false);
+
+        // when no designation is selected.
+        PromoteEmployeePage.clickOnSubmitBtn();
+        cy.wait(1000);
+        PromoteEmployeePage.selectDesignationDrp
+        .invoke('prop', 'validationMessage') 
+        .should('equal', 'Please select an item in the list.');
+
+
+    });
+
+
+    it.only("HRMIS_4: Verify Validation message appears after clicking on submit button when no department or designation is selected", () => {
+        
+
+        const designations = ['Select Designation','CEO & Chief Architect','Co Founder & COO','VP Sucess Manager & Sales','Chief People Officer','Piyush Dogra'];
+
+        cy.wait(1000);
+        selectDrpValueByText(PromoteEmployeePage.selectEmployeeDrp, "Auto Mation User", true, PromoteEmployeePage.selectEmployeeDrp);
+        PromoteEmployeePage.clickOnPromote();
+        PromoteEmployeePage.promotePopHeaderLbl.should('have.text','Promote Employee');
+        selectDrpValueByText(PromoteEmployeePage.selectDepartmentDrp, "GENRIC", false);
+
+        PromoteEmployeePage.getdesgnationOptions().then((options) => {
+            const optionTexts = [...options].map(option => option.innerText.trim()); // Extract all option texts
+            optionTexts.forEach((text,index) => {
+            expect(text).to.deep.equal(designations[index]); 
+            });
+          });
+
+
+        })
+       
+    
+
+
+});
